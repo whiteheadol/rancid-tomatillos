acc7d9ddc69c65ab456abf404188c06716f70cce
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-12 14:56:29 -0600

    Set up basic state and app render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+import movieData from './movieData.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // console.log(movieData.movies)
+  constructor() {
+    super();
+    this.state ={
+      movies: movieData.movies
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Rancid Tomatillos</h1>
+        <MoviesContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// Set up basic App structure
+// Find a place to store movieData (state)
+
+// Make components (MoviesContainer & Movie)
+// Pass down movieData information
+
+
+
+//   render() {
+//     return (
+//       <main className='App'>
+//         <h1>IdeaBox</h1>
+//         <Form addIdea={this.addIdea} />
+//         {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2> }
+//         <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea}/>
+//       </main>
+//     )
+//   }
+// }
