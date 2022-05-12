e19f669c595281a38b6b4190f865c6ce1d3ef22f
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-12 16:00:55 -0600

    Add thumbnail image styling

--- src/Movie.js
+++ src/Movie.js
@@ -5,8 +5,8 @@ const Movie = ({ poster, title, rating }) => {
   return (
     <div className="movie-thumbnail">
       <div className="movie-poster" style={{ backgroundImage: `url(${poster})` }}></div>
-      <h2>{title}</h2>
-      <p>{rating}</p>
+      <h2 className="movie-title">{title}</h2>
+      <p className="movie-rating">{rating}</p>
     </div>
   )
 };
