ba06e887925129da200ea62a94f181f1bdc8942e
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-13 19:06:03 -0600

    Add styling for movie details page

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './MovieDetails.css';
 
-const MovieDetails = ({props, moviesContainerHandler}) => {
+const MovieDetails = ({currentMovie, moviesContainerHandler}) => {
   return (
     <div className="movie-details">
-      <p>{props.movie.title}</p>
-      <p>{props.movie.overview}</p>
-      <p>{props.movie.average_rating}</p>
-      <p>{props.movie.release_date}</p>
-      <p>{props.movie.genres}</p>
-      <p>{props.movie.budget}</p>
-      <p>{props.movie.revenue}</p>
-      <p>{props.movie.runtime}</p>
-      <p>{props.movie.tagline}</p>
-      <button className="back-to-homepage" onClick={() => moviesContainerHandler()}></button>
+      <div className="movie-backdrop"  style={{ backgroundImage: `url(${currentMovie.movie.backdrop_path})` }}>
+        <div className="movie-text">
+          <p className="title"><strong>{currentMovie.movie.title}: </strong>{currentMovie.movie.tagline}</p>
+          <p>{currentMovie.movie.overview}</p>
+          <p><strong>Rating: </strong>{currentMovie.movie.average_rating}</p>
+          <p><strong>Release Date: </strong>{currentMovie.movie.release_date}</p>
+          <p><strong>Genres: </strong>{currentMovie.movie.genres}</p>
+          <p><strong>Budget: </strong>{currentMovie.movie.budget}</p>
+          <p><strong>Revenue: </strong>{currentMovie.movie.revenue}</p>
+          <p><strong>Runtime: </strong>{currentMovie.movie.runtime} min.</p>
+        </div>
+      </div>
+      <button className="back-to-homepage" onClick={() => moviesContainerHandler()}><strong>Back to Home</strong></button>
     </div>
   )
 };
