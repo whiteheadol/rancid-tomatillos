e6cdd7f4fd35e1991815cf4bbf436e2631d146c3
Kevin King <kevinturingking@gmail.com>
2022-05-16 17:48:58 -0600

    imported Link and wrapped poster in Link

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './Movie.css';
 
 const Movie = ({ poster, title, rating, currentMovieHandler, id }) => {
   return (
     <div className="movie-thumbnail" onClick={() => currentMovieHandler(id)} >
+    <Link className="movie-link" to={`/${id}`}>
       <div className="movie-poster" style={{ backgroundImage: `url(${poster})` }}></div>
+    </Link>
       <h2 className="movie-title">{title}</h2>
       <p className="movie-rating">Rating: {rating.toFixed(2)}</p>
     </div>
