33d7954afa324ef19d5f43ffe8043f78dad04226
Kevin King <kevinturingking@gmail.com>
2022-05-16 19:28:10 -0600

    refactor

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './MovieDetails.css';
 
 const MovieDetails = ({currentMovie, moviesContainerHandler, error}) => {
+  // const location = useParams()
+  // console.log(location)
   return (
     <div className="movie-details">
       <div className="movie-backdrop"  style={{ backgroundImage: `url(${currentMovie.backdrop_path})` }}>
