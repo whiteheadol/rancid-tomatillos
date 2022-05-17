a89e5554dad18149fda63e662706b5dc81529581
Kevin King <kevinturingking@gmail.com>
2022-05-16 19:27:30 -0600

    Notes for finding params

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import './Movie.css';
 
 const Movie = ({ poster, title, rating, currentMovieHandler, id }) => {
