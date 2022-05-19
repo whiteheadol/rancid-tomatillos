b0e5dafc802a31fb21c7c68337fefe601c2381b0
Kevin King <kevinturingking@gmail.com>
2022-05-18 19:01:37 -0600

    Add some testing

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -17,26 +17,36 @@ describe('example to-do app', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://example.cypress.io/todo')
+    cy.visit('http://localhost:3000/')
   })
 
-  it('displays two todo items by default', () => {
+  it('displays homepage by default', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
-    cy.get('.todo-list li').should('have.length', 2)
-
+    cy.contains('Rancid Tomatillos')
+    cy.get('.movie-thumbnail').should('have.length', 40)
+    cy.get('.movie-thumbnail').first().should('have.text', 'Money PlaneRating: 6.88')
+    cy.get('.movie-thumbnail').last().should('have.text', 'I Still BelieveRating: 3.83')
     // We can go even further and check that the default todos each contain
     // the correct text. We use the `first` and `last` functions
     // to get just the first and last matched elements individually,
     // and then perform an assertion with `should`.
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   })
 
-  it('can add new todo items', () => {
+  it('Should be able to click on a movie thumbnail to see its details', () => {
     // We'll store our item text in a variable so we can reuse it
-    const newItem = 'Feed the cat'
+    cy.get('#694919').click()
+    // cy.contains('Money Plane:')
+    // cy.contains("A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.")
+    // cy.contains('Rating: 6.875')
+    // cy.contains('Release Date: 2020 - 09 - 29')
+    // cy.contains('Genres: Action')
+    // cy.contains('Budget: 0')
+    // cy.contains('Revenue: 0')
+    // cy.contains('Runtime: 82 min.')
 
     // Let's get the input element and use the `type` command to
     // input our new list item. After typing the content of our item,
@@ -44,20 +54,20 @@ describe('example to-do app', () => {
     // This input has a data-test attribute so we'll use that to select the
     // element in accordance with best practices:
     // https://on.cypress.io/selecting-elements
-    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
+    // cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
 
     // Now that we've typed our new item, let's check that it actually was added to the list.
     // Since it's the newest item, it should exist as the last element in the list.
     // In addition, with the two default items, we should have a total of 3 elements in the list.
     // Since assertions yield the element that was asserted on,
     // we can chain both of these assertions together into a single statement.
-    cy.get('.todo-list li')
-      .should('have.length', 3)
-      .last()
-      .should('have.text', newItem)
+    // cy.get('.todo-list li')
+    //   .should('have.length', 3)
+    //   .last()
+    //   .should('have.text', newItem)
   })
 
-  it('can check off an item as completed', () => {
+  it.skip('can check off an item as completed', () => {
     // In addition to using the `get` command to get an element by selector,
     // we can also use the `contains` command to get an element by its contents.
     // However, this will yield the <label>, which is lowest-level element that contains the text.
@@ -91,7 +101,7 @@ describe('example to-do app', () => {
         .check()
     })
 
-    it('can filter for uncompleted tasks', () => {
+    it.skip('can filter for uncompleted tasks', () => {
       // We'll click on the "active" button in order to
       // display only incomplete items
       cy.contains('Active').click()
@@ -108,7 +118,7 @@ describe('example to-do app', () => {
       cy.contains('Pay electric bill').should('not.exist')
     })
 
-    it('can filter for completed tasks', () => {
+    it.skip('can filter for completed tasks', () => {
       // We can perform similar steps as the test above to ensure
       // that only completed tasks are shown
       cy.contains('Completed').click()
@@ -121,7 +131,7 @@ describe('example to-do app', () => {
       cy.contains('Walk the dog').should('not.exist')
     })
 
-    it('can delete all completed tasks', () => {
+    it.skip('can delete all completed tasks', () => {
       // First, let's click the "Clear completed" button
       // `contains` is actually serving two purposes here.
       // First, it's ensuring that the button exists within the dom.
--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom';
 const Movie = ({ poster, title, rating, currentMovieHandler, id }) => {
   return (
     <div className="movie-thumbnail" onClick={() => currentMovieHandler(id)} >
-      <NavLink to={`/${id}`} className="nav-link"><div className="movie-poster" style={{ backgroundImage: `url(${poster})` }}></div></NavLink>
+      <NavLink to={`/${id}`} className="nav-link"><div id={id} className="movie-poster" style={{ backgroundImage: `url(${poster})` }}></div></NavLink>
       <h2 className="movie-title">{title}</h2>
       <p className="movie-rating">Rating: {rating.toFixed(2)}</p>
     </div>
